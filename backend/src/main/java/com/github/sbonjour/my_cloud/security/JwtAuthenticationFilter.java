800847625c72b8b0f2d96ab152511b036934a51e
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-02 14:53:33 +0200

    update de SecurityConfig (creation securityFilterChain), creation de jwtAuthenticationFilter

---
+++ backend/src/main/java/com/github/sbonjour/my_cloud/security/JwtAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.github.sbonjour.my_cloud.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        if (jwtService.isTokenValid(token)) {
+            UUID userId = jwtService.extractUserId(token);
+
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    userId, null, Collections.emptyList());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
