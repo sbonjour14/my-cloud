b3ba8f6524fec2d60dec5ad1b83d004d9e2e71c1
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-13 20:44:29 +0200

    initialisation du theme dans l'app, annulation du scroll horizontal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import  {ProtectedRoute} from "./components/layout/ProtectedRoute";
-import { LoginPage } from "./pages/LoginPage";
-import  { RegisterPage } from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import { HomePage } from "./pages/HomePage";
+import { useTheme } from "./hooks/useTheme";
 
 export default function App() {
+  useTheme();
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
