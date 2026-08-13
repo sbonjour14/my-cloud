74489a3e60f316b6e0c37463296b3dde0ca52896
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-13 20:43:41 +0200

    auth.ts : typage et ajout de la fonction logoutUser

--- frontend/src/lib/http-api/auth.ts
+++ frontend/src/lib/http-api/auth.ts
@@ -4,10 +4,14 @@ import type { LoginFormBody, RegisterFormBody, RegisterResponse } from "@/types"
 
 export async function registerUser(payload: RegisterFormBody) : Promise<RegisterResponse> {
     const {data : response} = await api.post("/auth/register", payload);
-    return response;
+    return response as RegisterResponse;
 }
 
-export async function loginUser(payload: LoginFormBody) {
+export async function loginUser(payload: LoginFormBody) : Promise<string>{
     const { data } = await api.post("/auth/login", payload);
-    return data.token;
+    return data.token as string;
+}
+
+export function logoutUser() {
+    localStorage.removeItem("my-cloud-token")
 }
