805f3ed6cc37e9a8f928974e4c04109151d229a8
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-14 20:05:29 +0200

    ajout button back dans les pages login et register, badges soon pour les connexions via google et apple

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,20 @@
 import { LoginForm } from "@/components/form/LoginForm";
 import { Logo } from "@/components/ui/logo";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-        export default function LoginPage() {
+export default function LoginPage() {
+  const navigate = useNavigate();
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="relative flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="absolute left-0 top-0 p-2 text-foreground cursor-pointer"
+        onClick={() => navigate(-1)}>
+        <ArrowLeft/>
+      </div>
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <Logo/>
+            <Logo />
           </div>
           Sbonjour Inc.
         </a>
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,9 +1,16 @@
 import { RegisterForm } from "@/components/form/RegisterForm";
 import { Logo } from "@/components/ui/logo";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
+    const navigate = useNavigate();
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+            <div className="absolute left-0 top-0 p-2 text-foreground cursor-pointer"
+                onClick={() => navigate(-1)}>
+                <ArrowLeft />
+            </div>
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <a href="#" className="flex items-center gap-2 self-center font-medium">
                     <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
