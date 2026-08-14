ec51a81757256b2e43a9760c920d98a1b5783446
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-14 20:04:01 +0200

    fix page blanche lors des F5 navigateur

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import  {ProtectedRoute} from "./components/layout/ProtectedRoute";
+import { ProtectedRoute } from "./components/layout/ProtectedRoute";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 import { useTheme } from "./hooks/useTheme";
 
 export default function App() {
@@ -11,11 +12,13 @@ export default function App() {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/" element={<HomePage/>}/>
 
       <Route element={<ProtectedRoute />}>
-
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
       </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
