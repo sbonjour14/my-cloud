6ee8c162c7fbed5f2d550995a2e84f0665c107d6
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-15 16:24:20 +0200

    feat(frontend): add uploadFile API call, rename getUserTotalStorageUsed to getStorageUsed

--- frontend/src/lib/http-api/media.ts
+++ frontend/src/lib/http-api/media.ts
@@ -2,7 +2,16 @@ import { api } from "../axios";
 
 
 
-export async function getUserTotalStorageUsed() : Promise<number> {
+export async function getStorageUsed() : Promise<number> {
     const {data:response} = await api.get("/mediaAssets/size")
     return response;
 } 
+
+export async function uploadFile(file: File): Promise<void> {
+  const formData = new FormData()
+  formData.append("file", file)
+
+  await api.post("/mediaAssets", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  })
+}
