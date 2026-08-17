7f338b652969fc9a5b78b548f10b8ed4710ae1b4
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-17 19:33:21 +0200

    feature(frontend): add TestPage to test api calls and components

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,3 @@
-import { Link } from "react-router-dom";
-
 export function HomePage() {
-    return (
-        <div className="bg-muted w-full min-h-screen">
-            <Link to={"/register"}> go to register</Link>
-            <Link to={"/login"}> go to login</Link>
-        </div>
-    )
+    return <>home page</>
 }
---
+++ frontend/src/pages/TestPage.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { UploadDialog } from "@/components/ui/upload-dialog";
+import { useUploadFile } from "@/hooks/useUploadFile";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+
+export function TestPage() {
+    const { upload, isOpen, status, progress, close } = useUploadFile();
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const fichier = event.target.files?.[0];
+        if (!fichier) return;
+        upload(fichier);
+        event.target.value = "";
+    };
+
+    return (
+        <div className="bg-muted w-full min-h-screen flex flex-col gap-5 items-center">
+            <Link to={"/register"}> go to register</Link>
+            <Link to={"/login"}> go to login</Link>
+            <input
+                ref={inputRef}
+                className="hidden"
+                type="file"
+                onChange={handleFileChange}
+            />
+            <Button onClick={() => inputRef.current?.click()}>
+                chose a file
+            </Button>
+            <UploadDialog
+                open={isOpen}
+                progress={progress}
+                status={status}
+                onClose={close}
+            />
+        </div>
+    )
+}
