0d71076622cc812a0c5af7b1e07a593d0ff94446
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-18 23:49:02 +0200

    feature: add api url in axios

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "/api"
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+
 })
+
+console.log(import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("my-cloud-token");
