6d6cd6367fb3673acf03c1feb25e2497568f2e6d
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-23 13:35:54 +0200

    fix: add localhost:5173/ to allowOrigins for dev

--- backend/src/main/java/com/github/sbonjour/my_cloud/config/CorsConfig.java
+++ backend/src/main/java/com/github/sbonjour/my_cloud/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://cloud.sbonjour.com"));
+        config.setAllowedOrigins(List.of("https://cloud.sbonjour.com", "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
