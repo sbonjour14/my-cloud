9ecd05ade8a6c9e2b967019f92b4144cb18bda32
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-23 12:16:16 +0200

    fix: call me()  to check if user is valid for protected routes

--- frontend/src/components/layout/ProtectedRoute.tsx
+++ frontend/src/components/layout/ProtectedRoute.tsx
@@ -1,11 +1,39 @@
+import { me } from "@/lib/http-api/user";
+import type { UserResponse } from "@/types";
+import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function ProtectedRoute() {
-    const token = localStorage.getItem("my-cloud-token");
+    const [isLoading, setIsloading] = useState<boolean>(true);
+    const [isValid, setisValid] = useState<boolean>(false);
 
-    if (!token) {
-        return <Navigate to="/login" replace />;
-    }
+    useEffect(() => {
+        async function checkUser() {
+            const token = localStorage.getItem("my-cloud-token");
+            if (!token) {
+                setisValid(false);
+                setIsloading(false);
+                return;
+            }
 
-    return <Outlet />;
+            const user: UserResponse = await me();
+            if(!user) {
+                setisValid(false);
+            } else {
+                setisValid(true);
+            }
+            setIsloading(false);
+        }
+
+        checkUser();
+
+    }, []);
+    if(isLoading)
+        return <p>loading</p>;
+
+    if(!isValid)
+        return <Navigate to={"/login"} replace/>
+
+    return <Outlet/>
+    
 }
