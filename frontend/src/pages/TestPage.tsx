ed2ed976ca47f2d66123447aa1177b867383460d
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-24 22:22:53 +0200

    use hooks to display storage used and media in test page

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,13 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+import { Spinner } from "@/components/ui/spinner";
+import { useMediaAssets } from "@/hooks/useMediaAssets";
 import { useUploadFile } from "@/hooks/useUploadFile";
 import { logoutUser } from "@/lib/http-api/auth";
 import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { CloudIcon } from "lucide-react";
+import { useUser } from "@/hooks/useUser";
+import { useStorageUsage } from "@/hooks/useStorageUsage";
 
 export function TestPage() {
     const navigate = useNavigate();
     const { upload } = useUploadFile();
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const {data: me, isLoading: meLoading} = useUser();
+    const {data: mediaAssets, isLoading: mediaLoading} = useMediaAssets();
+    const {data: storageUsed, isLoading: storageLoading} = useStorageUsage(me?.id);
+
+
+    const isLoading = meLoading && mediaLoading && storageLoading;
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const fichier = event.target.files?.[0];
@@ -33,6 +46,51 @@ export function TestPage() {
             <Button onClick={async () => {await logoutUser(); navigate("/login")}}>
                 log out
             </Button>
+
+            <div>
+                storage : {((storageUsed?? 0) / 1000000).toFixed(2)}MB
+            </div>
+
+            {
+                isLoading ? 
+                    <Spinner/> :
+
+                    !mediaAssets || mediaAssets.length == 0 ?
+                        <Empty className="border border-dashed">
+                            <EmptyHeader>
+                                <EmptyMedia variant="icon">
+                                    <CloudIcon />
+                                </EmptyMedia>
+                                <EmptyTitle>Cloud Storage Empty</EmptyTitle>
+                                <EmptyDescription>
+                                    Upload files to your cloud storage to access them anywhere.
+                                </EmptyDescription>
+                            </EmptyHeader>
+                            <EmptyContent>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => inputRef.current?.click()}
+                                >
+                                    Upload Files
+                                </Button>
+                            </EmptyContent>
+                        </Empty>
+
+
+                    :
+                        <div className="mt-5 grid grid-cols-2 gap-3">
+                            {mediaAssets.map(ma => (
+                                <div key={ma.id}>
+                                    <img
+                                        src={import.meta.env.VITE_API_URL + ma.url}
+                                        className="h-40 w-40 object-cover"
+                                    />
+                                </div>
+                            ))}
+                        </div>
+            }
+
         </div>
     )
 }
