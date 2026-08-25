bfa0b7315c561ca0458e2dcc90a4568f2aa8bd6a
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-25 20:17:15 +0200

    feat: postgres containers for tests

---
+++ backend/src/test/java/com/github/sbonjour/my_cloud/repository/AbstractPostgresContainerTest.java
@@ -0,0 +1,26 @@
+package com.github.sbonjour.my_cloud.repository;
+
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class AbstractPostgresContainerTest {
+
+    static final PostgreSQLContainer postgres =
+            new PostgreSQLContainer(DockerImageName.parse("postgres:18-alpine"));
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+}
