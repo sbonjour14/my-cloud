e88fe694e20142d58042f4a539cfd0ef39633b48
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-08-29 17:03:00 +0200

    test: add tests for StoredFileRepository.FindByMediaType

--- backend/src/test/java/com/github/sbonjour/my_cloud/repository/StoredFileRepositoryTests.java
+++ backend/src/test/java/com/github/sbonjour/my_cloud/repository/StoredFileRepositoryTests.java
@@ -2,6 +2,7 @@ package com.github.sbonjour.my_cloud.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -88,4 +89,50 @@ public class StoredFileRepositoryTests extends AbstractPostgresContainerTest {
         }
     }
 
+    @Nested
+    class FindByMediaType {
+
+        @Test
+        void shouldFindByMediaType_whenMediaTypeIsImage() {
+            StoredFile image1 = TestDataFactory.persistStoredFile(entityManager, "image1Checksum", MediaType.IMAGE, "image1MimType", 10, "/uploads/image1Checksum");
+            StoredFile image2 = TestDataFactory.persistStoredFile(entityManager, "image2Checksum", MediaType.IMAGE, "image2MimType", 10, "/uploads/image2Checksum");
+            TestDataFactory.persistStoredFile(entityManager, "video1Checksum", MediaType.VIDEO, "video1MimType", 10, "/uploads/video1Checksum");
+
+            List<StoredFile> found = repository.findByMediaType(MediaType.IMAGE);
+
+            assertThat(found).containsExactlyInAnyOrder(image1, image2);
+        }
+        
+        @Test
+        void shouldFindByMediaType_whenMediaTypeIsVideo() {
+            TestDataFactory.persistStoredFile(entityManager, "image1Checksum", MediaType.IMAGE, "image1MimType", 10, "/uploads/image1Checksum");
+            TestDataFactory.persistStoredFile(entityManager, "image2Checksum", MediaType.IMAGE, "image2MimType", 10, "/uploads/image2Checksum");
+            StoredFile video1 = TestDataFactory.persistStoredFile(entityManager, "video1Checksum", MediaType.VIDEO, "video1MimType", 10, "/uploads/video1Checksum");
+            StoredFile video2 = TestDataFactory.persistStoredFile(entityManager, "video2Checksum", MediaType.VIDEO, "video2MimType", 10, "/uploads/video2Checksum");
+
+            List<StoredFile> found = repository.findByMediaType(MediaType.VIDEO);
+
+            assertThat(found).containsExactlyInAnyOrder(video1, video2);
+        }
+        
+        @Test
+        void shouldNotFindByMediaType_whenMediaTypeIsImage() {
+            TestDataFactory.persistStoredFile(entityManager, "video1Checksum", MediaType.VIDEO, "video1MimType", 10, "/uploads/video1Checksum");
+            TestDataFactory.persistStoredFile(entityManager, "video2Checksum", MediaType.VIDEO, "video2MimType", 10, "/uploads/video2Checksum");
+
+            List<StoredFile> found = repository.findByMediaType(MediaType.IMAGE);
+
+            assertThat(found).isEmpty();
+        }
+        
+        @Test
+        void shouldNotFindByMediaType_whenMediaTypeIsVideo() {
+            TestDataFactory.persistStoredFile(entityManager, "image1Checksum", MediaType.IMAGE, "image1MimType", 10, "/uploads/image1Checksum");
+            TestDataFactory.persistStoredFile(entityManager, "image2Checksum", MediaType.IMAGE, "image2MimType", 10, "/uploads/image2Checksum");
+
+            List<StoredFile> found = repository.findByMediaType(MediaType.VIDEO);
+
+            assertThat(found).isEmpty();
+        }
+    }
 }
