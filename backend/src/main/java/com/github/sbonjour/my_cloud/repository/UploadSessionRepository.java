dd103b292bc779a8912bc75bafd52d4a2098e9ee
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-09-02 16:25:19 +0200

    feat: add UploadSessionRepository, with findyById and findByChecksum

---
+++ backend/src/main/java/com/github/sbonjour/my_cloud/repository/UploadSessionRepository.java
@@ -0,0 +1,17 @@
+package com.github.sbonjour.my_cloud.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.sbonjour.my_cloud.entity.UploadSession;
+
+public interface UploadSessionRepository extends JpaRepository<UploadSession, UUID>{
+    
+    public Optional<UploadSession> findById(UUID id);
+
+    public boolean existsByChecksum(String checksum);
+
+    
+}
