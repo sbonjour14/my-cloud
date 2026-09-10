57592b6388e288e5d6af69efc292a3c3066a420f
Steven Bonjour <steven.bonjour@etu.u-bordeaux.fr>
2026-09-10 15:54:15 +0200

    feat: button to test page in HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 export function HomePage() {
-    return <>home page</>
+	const navigate = useNavigate();
+	return (
+		<div className="flex-col gap-5 justify-center mt-10">
+			<Button variant={"default"}
+				onClick={() => navigate("/test")}
+			>
+				test
+			</Button>
+			<div>
+				home page
+			</div>
+		</div>
+	)
 }
